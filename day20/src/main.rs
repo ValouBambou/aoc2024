879047d8ab3719726ee76e391d65dddd8efb917b
Valentin Trophime <valentin.trophime@inria.fr>
2025-01-23 20:06:54 +0100

    day 20 done can be improved

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -1,34 +1,27 @@
 use std::{
-    collections::{HashMap, VecDeque},
+    collections::{HashMap, HashSet, VecDeque},
     ops::{Add, Mul},
 };
 
 const INPUT: &'static str = include_str!("../input.txt");
 
 fn main() {
-    let answer = part1(INPUT);
+    let answer = part1::<100>(INPUT);
     println!("Part 1 answer is {answer}");
-    let answer = part2(INPUT);
+    let answer = part2::<100>(INPUT);
     println!("Part 2 answer is {answer}");
 }
 
-fn part1(input: &'static str) -> u32 {
-    part_x(input, 2)
+fn part1<const N: u32>(input: &'static str) -> u32 {
+    part_x::<N>(input, 2)
 }
-fn part2(input: &'static str) -> u32 {
-    part_x(input, 20)
+fn part2<const N: u32>(input: &'static str) -> u32 {
+    part_x::<N>(input, 20)
 }
-fn part_x(input: &'static str, cheat_duration: u8) -> u32 {
-    let mut g = Grid::new(input);
+fn part_x<const N: u32>(input: &'static str, cheat_duration: i16) -> u32 {
+    let g = Grid::new(input);
     let base_cost = g.dijsktra();
-    let cheats = g.find_cheats(base_cost, cheat_duration);
-    let mut debug_v = cheats.iter().collect::<Vec<_>>();
-    debug_v.sort_by_key(|t| t.0);
-    for (gain, n_cheats) in debug_v {
-        println!("{n_cheats} cheats saving {gain} picoseconds");
-    }
-
-    cheats.into_values().sum()
+    g.dijsktra_cheat::<N>(base_cost, cheat_duration)
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Default, Hash)]
@@ -82,145 +75,72 @@ impl Grid {
         debug_assert!(idx < n * n);
         Coord([(idx % n) as i16, (idx / n) as i16])
     }
-    fn find_cheats(&mut self, base_score: u32, cheat_duration: u8) -> HashMap<u32, u32> {
-        let mut visited = vec![false; self.grid.len()];
-        let mut cheats = HashMap::new();
-        let node = MazeNode2 {
-            cost: 0,
-            position: self.pos(self.start),
-            cheat: Cheat::Usable,
-        };
-        visited[self.start] = true;
-        self.dfs_cheat(node, &mut cheats, &mut visited, base_score, cheat_duration);
-        let mut occurences = HashMap::new();
-        for v in cheats.into_values() {
-            *occurences.entry(v).or_insert(0) += 1;
-        }
-        occurences
-    }
-    fn cheat_neighbors(&self, cheat_duration: u8, cur: Coord) -> Vec<(Coord, usize)> {
-        let cheat_duration = cheat_duration as i16;
-        let mut res = vec![];
-        for ix in (-cheat_duration)..=cheat_duration {
-            for iy in (-cheat_duration)..=cheat_duration {
-                if ix + iy == 0 || ix.abs() + iy.abs() > cheat_duration {
-                    continue;
-                }
+    fn cheat_neighbors(
+        &self,
+        cheat_duration: i16,
+        cur: Coord,
+    ) -> impl Iterator<Item = (Coord, usize)> + '_ {
+        ((-cheat_duration)..=cheat_duration).flat_map(move |ix| {
+            ((-cheat_duration)..=cheat_duration).filter_map(move |iy| {
+                let dist = ix.abs() + iy.abs();
                 let nei = cur + Coord([ix, iy]);
-                let idx;
-                if (0..self.n).contains(&nei.0[0])
-                    && (0..self.n).contains(&nei.0[1])
-                    && self.grid[{
-                        idx = self.idx(&nei);
-                        idx
-                    }] != b'#'
+                if !((1..=cheat_duration).contains(&dist)
+                    && (0..self.n).contains(&nei.0[0])
+                    && (0..self.n).contains(&nei.0[1]))
                 {
-                    res.push((nei, idx));
+                    return None;
                 }
-            }
-        }
-        res
+                let idx = self.idx(&nei);
+                (self.grid[idx] != b'#').then_some((nei, idx))
+            })
+        })
     }
-    fn dfs_cheat(
-        &mut self,
-        cur: MazeNode2,
-        cheats: &mut HashMap<[Coord; 2], u32>,
-        visited: &mut [bool],
-        base_score: u32,
-        cheat_duration: u8,
-    ) {
-        if self.idx(&cur.position) == self.end {
-            match cur.cheat {
-                Cheat::Used(x) => {
-                    cheats.insert(x, base_score - cur.cost);
-                }
-                _ => (),
-            }
-            return;
-        }
-        #[cfg(not(test))]
-        const N: u32 = 100;
-        #[cfg(test)]
-        const N: u32 = 50;
-        if matches!(cur.cheat, Cheat::Usable) {
-            for (position, nei_idx) in self.cheat_neighbors(cheat_duration, cur.position) {
-                let cost = cur.cost + position.dist(cur.position);
-                if cost <= base_score - N && (!visited[nei_idx]) {
-                    let node = MazeNode2 {
-                        cost,
-                        position,
-                        cheat: Cheat::Used([cur.position, position]),
-                    };
-                    visited[nei_idx] = true;
-                    self.dfs_cheat(node, cheats, visited, base_score, cheat_duration);
-                    visited[nei_idx] = false;
-                }
-            }
-        }
-        for next_dir in DIRECTIONS {
-            let position = cur.position + next_dir;
-            let cost = cur.cost + 1;
-            let nei_idx = self.idx(&position);
-            if cost <= base_score - N && self.grid[nei_idx] != b'#' && (!visited[nei_idx]) {
-                let node = MazeNode2 {
-                    cost,
-                    position,
-                    cheat: cur.cheat,
-                };
-                visited[nei_idx] = true;
-                self.dfs_cheat(node, cheats, visited, base_score, cheat_duration);
-                visited[nei_idx] = false;
-            }
-        }
-    }
-    fn dijsktra(&mut self) -> u32 {
-        #[derive(Clone, Copy, PartialEq, Eq)]
-        struct MazeNode {
-            cost: u32,
-            position: Coord,
-        }
+    fn dijsktra(&self) -> Vec<usize> {
         let mut queue = VecDeque::new();
         let mut visited = vec![false; self.grid.len()];
+        let mut prev = vec![usize::MAX; self.grid.len()];
         visited[self.start] = true;
-        queue.push_back(MazeNode {
-            cost: 0,
-            position: self.pos(self.start),
-        });
-        let mut score = u32::MAX;
-        while let Some(cur) = queue.pop_front() {
-            let cur_idx = self.idx(&cur.position);
+        queue.push_back((0, self.pos(self.start)));
+        while let Some((cur_cost, cur_pos)) = queue.pop_front() {
+            let cur_idx = self.idx(&cur_pos);
             if cur_idx == self.end {
-                score = cur.cost;
                 break;
             }
             for next_dir in DIRECTIONS {
-                let nei = cur.position + next_dir;
-                let nei_cost = cur.cost + 1;
+                let nei = cur_pos + next_dir;
+                let nei_cost = cur_cost + 1;
                 let nei_idx = self.idx(&nei);
                 if self.grid[nei_idx] != b'#' && (!visited[nei_idx]) {
                     visited[nei_idx] = true;
-                    queue.push_back(MazeNode {
-                        cost: nei_cost,
-                        position: nei,
-                    });
+                    prev[nei_idx] = cur_idx;
+                    queue.push_back((nei_cost, nei));
                 }
             }
         }
-        score
+        let mut cur = self.end;
+        let mut path = Vec::new();
+        while self.start != cur {
+            path.push(cur);
+            cur = prev[cur];
+        }
+        path.push(self.start);
+        path.reverse();
+        path
     }
-}
-
-#[derive(Clone, Copy, PartialEq, Eq)]
-struct MazeNode2 {
-    cost: u32,
-    position: Coord,
-    cheat: Cheat,
-}
-
-#[derive(Clone, Copy, PartialEq, Eq, Hash)]
-enum Cheat {
-    Usable,
-    Used([Coord; 2]),
+    fn dijsktra_cheat<const N: u32>(&self, base_path: Vec<usize>, cheat_duration: i16) -> u32 {
+        let mut cheats = HashSet::new();
+        for (i, &cur) in base_path.iter().enumerate() {
+            let cur_pos = self.pos(cur);
+            for (nei_pos, nei) in self.cheat_neighbors(cheat_duration, cur_pos) {
+                let dist = nei_pos.dist(cur_pos);
+                let i2 = base_path.iter().position(|&idx| idx == nei);
+                if i2.is_some_and(|i2| i2.saturating_sub(i) >= (dist + N) as usize) {
+                    cheats.insert([cur_pos, nei_pos]);
+                }
+            }
+        }
+        cheats.len() as u32
+    }
 }
 
 #[cfg(test)]
@@ -244,11 +164,11 @@ const EXAMPLE: &'static str = "
 
 #[test]
 fn test1() {
-    // only one cheat save 50 picosecs or more
-    assert_eq!(part1(EXAMPLE), 1);
+    let total = 14 + 14 + 2 + 4 + 2 + 3 + 1 + 1 + 1 + 1 + 1;
+    assert_eq!(part1::<2>(EXAMPLE), total);
 }
 #[test]
 fn test2() {
     let total = 32 + 31 + 29 + 39 + 25 + 23 + 20 + 19 + 12 + 14 + 12 + 22 + 4 + 3;
-    assert_eq!(part2(EXAMPLE), total);
+    assert_eq!(part2::<50>(EXAMPLE), total);
 }
